b5e40c3645df448a2e75c48d5f024ce68a33b23a
dhiaaziz <azizwkwkwk@gmail.com>
2022-07-04 00:04:42 +0700

    init backend

---
+++ aplikasi-data-produk-deskripsi/backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+require('dotenv').config()
+
+
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  )
+);
+
+
